5bdf456fb83602941e18ce472d380a1ca6566e70
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-30 09:11:11 -0600

    adding initial release

---
+++ README.md
@@ -0,0 +1,37 @@
+# action-repository-dispatch
+
+Summary: This github action is used to create an event for github action repository dispatch.
+
+![ci](https://github.com/conventional-changelog/standard-version/workflows/ci/badge.svg)
+[![version](https://img.shields.io/badge/version-1.x-yellow.svg)](https://semver.org)
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#Features)
+* [Assumptions](#Assumptions)
+* [Usage](#usage)
+* [Project Status](#project-status)
+
+## General Information
+- action-repository-dispatch
+
+## Technologies Used
+- python script
+- github actions
+
+## Features
+
+* action used to create an event for gh repository dispatch
+* client payload includes version, ref and sha
+
+## Assumptions
+
+* n/a
+
+## Usage
+
+* in progress
+
+## Project Status
+Project is: _in_progress_ 
---
+++ action-repository-dispatch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# usage: python action-repository-dispatch.py
+# action-repository-dispatch.py action repository dispatch
+###############################################################################
+
+import requests
+import os
+import json
+
+def main():
+    org_name = os.environ.get("ORG_NAME")
+    repo_name = os.environ.get("REPO_NAME")
+    github_token = os.environ.get("GITHUB_TOKEN")
+    event = os.environ.get("EVENT")
+    github_ref = os.environ.get("GITHUB_REF")
+    github_sha = os.environ.get("GITHUB_SHA")
+    version = os.environ.get("VERSION")
+
+    url = "https://api.github.com/repos/" + org_name + "/" + repo_name + "/dispatches"
+    print(url)
+
+    payload = {
+        "event_type": event,
+        "client_payload": {
+            "version": version,
+            "ref": github_ref,
+            "sha": github_sha,
+        }
+    }
+
+    headers = {
+      "Accept": "application/vnd.github.v3+json",
+      "Authorization": f"token {github_token}" ,
+    }
+
+    response = requests.post(url=url, json=payload, headers=headers)
+    print(response)
+main()
+
